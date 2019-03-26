a92f9131da0e2fe98ab49e49d5a5354044cd0a4d
Pedro Beltran <pepeu.beltran@gmail.com>
2019-03-25 22:10:05 -0300

    Add files via upload

---
+++ Making of/saving_data.py
@@ -0,0 +1,18 @@
+import json
+
+# Saving results on a file - Functions
+def save_data(filename, test_accuracy, acc_by_class, \
+              epochs, no_improvement_in, mbs, eta):
+    data = {"test_accuracy": test_accuracy,
+            "acc_by_class": acc_by_class,
+            #"cost": evaluation_cost,
+            "eta": eta,
+            "mini-batch size": mbs,
+            "epochs": epochs,
+            "no_improvement_in": no_improvement_in}
+
+    f = open(filename, "w")
+    json.dump(data, f)
+    f.close()
+
+#*************************************
